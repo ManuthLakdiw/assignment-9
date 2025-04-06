84f74f594c56a2340eb7899347ace7622c8343d3
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2025-04-06 22:34:26 +0530

    added home section script

--- js/homeSection.js
+++ js/homeSection.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    const audio = $("#backgroundMusic")[0];
+    const backGroundAudio = $("#backgroundMusic")[0];
 
     let volUp = $("#volUp");
     let volDown =  $("#volDown");
@@ -7,15 +7,16 @@ $(document).ready(function(){
     volUp.css("display","none");
 
     volUp.on("click",function(){
+        backGroundAudio.pause();
         volUp.css("display","none");
         volDown.css("display","block");
-        audio.pause();
     });
 
     volDown.on("click",function(){
+        backGroundAudio.play(); 
         volUp.css("display","block");
         volDown.css("display","none");
-        audio.play();
+     
     });
 
     $("#playBtn").on("click",function(){
